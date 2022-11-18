1c1847a811156a3347d6795bbf2088cd9e79ec90
erick macias <103471146+lt0000er@users.noreply.github.com>
2022-11-17 23:11:27 -0500

    Add files via upload
    API-DOGS

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
+import './App.css';
+import select  from ' /componente/initldog'
 
-//import './App.css';
-import {TodoCounter} from './TodoCounter'
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
+const inicialdog = {
+Image: ""
+ razas: {
+   id: 0,
+   name: ""
+}
 
- const todos = [ 
-
-  {description: 'Aprender React native', completed: false},
-  {description: 'Aprender Angular', completed: false},
-  {description: 'Aprender Vue', completed: false},
-  {description: 'Aprender Diseño', completed: false},
-
-]
+}
 
 function App() {
   return (
    <>
-     <TodoCounter/>
-     <TodoSearch/>
+     <initldog/>
 
-     <TodoList>
-       {todos.map((todo) => (
-         <TodoItem key={todo.description} text={todo.description} />
-         
-       ))} 
+      
 
-      </TodoList>
-
-       <CreateTodoButton/>
-  
-  </>
+   </>
   );
 }
 
