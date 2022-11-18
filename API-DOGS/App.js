3e8d0cb66c070a884ad1b9f649b79f9d88cb4657
erick macias <103471146+lt0000er@users.noreply.github.com>
2022-11-17 23:14:54 -0500

    Add files via upload

---
+++ API-DOGS/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import select  from ' /componente/initldog'
+
+const inicialdog = {
+Image: ""
+ razas: {
+   id: 0,
+   name: ""
+}
+
+}
+
+function App() {
+  return (
+   <>
+     <initldog/>
+
+      
+
+   </>
+  );
+}
+
+export default App;
